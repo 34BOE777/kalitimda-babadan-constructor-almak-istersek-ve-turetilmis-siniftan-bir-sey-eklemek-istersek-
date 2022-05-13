fe5fe4fe092782c6541a4fb971dfb8f5aba86452
34BOE777 <98213674+34BOE777@users.noreply.github.com>
2022-05-13 12:58:26 +0300

    Add files via upload

---
+++ cu.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+
+
+class hayvan{
+private:
+	string renk;
+public:
+	hayvan(string renk):renk(renk){}
+
+	hayvan(){}
+};
+
+
+
+
+
+class kedi:public hayvan{
+private:
+	bool meow;
+public:
+	bool getmeow(){
+		return meow;
+	}
+	kedi(string renk,bool meow):hayvan(renk),meow(meow){}
+	
+
+};
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
+
+
+
+
+
+
